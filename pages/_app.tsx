ce44a6cec047d3f751af28510925e1481306fcc8
Nathan Luong <luongn4@mcmaster.ca>
2024-04-05 18:04:49 -0400

    backend-2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import "../styles/loadingAnimation.css";
 import "../styles/analytic.css";
 import "../styles/mdBlogs.module.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
@@ -21,9 +20,7 @@ function MyApp({
   return (
     <ThemeProvider attribute="class">
       <SessionProvider session={pageProps.session} refetchOnWindowFocus={true}>
-        <Layout>
           <Component {...pageProps} />
-        </Layout>
       </SessionProvider>
     </ThemeProvider>
   );
