ddc5e504474d79939b28a2cc35d9194a0fcf61f4
Nathan Luong <luongn4@mcmaster.ca>
2024-05-18 22:17:56 -0400

    Added error log for revalidate function

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -14,6 +14,7 @@ export default async function handler(
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send("Error revalidating");
+    // Return the error as well
+    return res.status(500).json({ error: err });
   }
 }
